150d495574c3dfd4de77e7b6074201c53370b571
mccoinj6975 <47360864+mccoinj6975@users.noreply.github.com>
2019-02-05 14:13:57 -0500

    Add files via upload
    First Python assignment

---
+++ MyFirstPythonProgram_McCoin.py
@@ -0,0 +1,10 @@
+ # This program is to practice and learn Python. The program will
+ # run and then print out various sentences in the run screen. 
+ # Date: 24 January 2019
+ # CTI-110 Tutorial 1 - My First Python Program
+ # Name: Jessica McCoin 
+ #
+
+print('This is a test of IDLE.')
+
+ 
